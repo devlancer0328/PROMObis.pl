606ebb33bfe3934f456a51df6b9638ec31a5c766
devlancer0328 <topdev0328@proton.me>
2023-05-28 12:55:38 +0200

    handle submenu on mobile

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,6 +1,6 @@
 const hamburger = document.querySelector('header #nav .top-nav .hamburger');
 const menu = document.querySelector('header #nav .bottom-nav');
-const menu_li = document.querySelector('header #nav .bottom-nav .left-container ul li .bottom-nav-sublist ul li');
+const menu_li = document.querySelectorAll('header #nav .bottom-nav .left-container ul li .bottom-nav-sublist ul li');
 
 
 hamburger.addEventListener('click', () => {
@@ -8,7 +8,8 @@ hamburger.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
 
-menu_li.addEventListener('click', () => {
-    menu_li.style.display="flex";
-});
+
+menu_li.forEach(e => e.addEventListener('click', () => {
+    e.classList.toggle('active');
+}));
 
