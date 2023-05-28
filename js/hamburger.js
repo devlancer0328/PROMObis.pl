d2f123ac8efb22004ec517c88969353c46297427
devlancer0328 <topdev0328@proton.me>
2023-05-28 12:30:16 +0200

    handle hamburger and mobile menu

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,8 +1,14 @@
 const hamburger = document.querySelector('header #nav .top-nav .hamburger');
 const menu = document.querySelector('header #nav .bottom-nav');
+const menu_li = document.querySelector('header #nav .bottom-nav .left-container ul li .bottom-nav-sublist ul li');
+
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     menu.classList.toggle('active');
+});
+
+menu_li.addEventListener('click', () => {
+    menu_li.style.display="flex";
 });
 
