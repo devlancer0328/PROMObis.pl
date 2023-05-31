bd36128615429d32a32555495023dd032a7a0aeb
devlancer0328 <topdev0328@proton.me>
2023-05-31 21:51:54 +0200

    add actual year script to footer

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 const hamburger = document.querySelector('header #nav .top-nav .hamburger');
 const menu = document.querySelector('header #nav .bottom-nav');
 const menu_li = document.querySelectorAll('header #nav .bottom-nav .left-container ul li .bottom-nav-sublist ul li');
-
+const footer_year = document.querySelector('.footer-bottom span');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     menu.classList.toggle('active');
 });
 
-
 menu_li.forEach(e => e.addEventListener('click', () => {
     e.classList.toggle('active');
 }));
 
+footer_year.innerHTML = new Date().getFullYear();
