3b0563e8ce878522df918e5552d61f4cfe2a8295
devlancer0328 <topdev0328@proton.me>
2023-06-13 22:41:58 +0200

    handle modal

--- js/script.js
+++ js/script.js
@@ -13,3 +13,17 @@ menu_li.forEach(e => e.addEventListener('click', () => {
 }));
 
 footer_year.innerHTML = new Date().getFullYear();
+
+const modal = document.querySelector('.account-add-a-new-address-container');
+const close_icon = document.querySelector('.account-add-a-new-address-container .x-close-icon');
+const add_address_btn = document.querySelector('.account-add-address-container .account-change-btn .add-address');
+
+add_address_btn.addEventListener('click', () => {
+    modal.classList.toggle('active');
+    document.body.classList.toggle('open-modal');
+});
+
+close_icon.addEventListener('click', () => {
+    modal.classList.toggle('active');
+    document.body.classList.toggle('open-modal'); 
+});
