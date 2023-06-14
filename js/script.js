bc3c82a0aefc5717fe72a369475a8f5762c947ae
devlancer0328 <topdev0328@proton.me>
2023-06-14 17:19:31 +0200

    handle and style all modals in account section

--- js/script.js
+++ js/script.js
@@ -14,16 +14,95 @@ menu_li.forEach(e => e.addEventListener('click', () => {
 
 footer_year.innerHTML = new Date().getFullYear();
 
-const modal = document.querySelector('.account-add-a-new-address-container');
-const close_icon = document.querySelector('.account-add-a-new-address-container .x-close-icon');
+/* Modal Add Address */
+const modal_add_address = document.querySelector('.account-add-a-new-address-container');
+const close_add_icon = document.querySelector('.account-add-a-new-address-container .x-close-icon');
 const add_address_btn = document.querySelector('.account-add-address-container .account-change-btn .add-address');
 
 add_address_btn.addEventListener('click', () => {
-    modal.classList.toggle('active');
+    modal_add_address.classList.toggle('active');
     document.body.classList.toggle('open-modal');
 });
 
-close_icon.addEventListener('click', () => {
-    modal.classList.toggle('active');
+close_add_icon.addEventListener('click', () => {
+    modal_add_address.classList.toggle('active');
+    document.body.classList.toggle('open-modal'); 
+});
+
+/* Modal Change Address */
+const modal_change_address = document.querySelector('.account-change-address-container');
+const close_change_address_icon = document.querySelector('.account-change-address-container .x-close-icon');
+const change_address_btn = document.querySelector('.account-address-container.location .account-change-btn .change-btn');
+
+change_address_btn.addEventListener('click', () => {
+    modal_change_address.classList.toggle('active');
+    document.body.classList.toggle('open-modal');
+});
+
+close_change_address_icon.addEventListener('click', () => {
+    modal_change_address.classList.toggle('active');
+    document.body.classList.toggle('open-modal'); 
+});
+
+/* Modal Change Delivery Address */
+const modal_change_delivery_address = document.querySelector('.account-change-delivery-address-container');
+const change_delivery_address_icon = document.querySelector('.account-change-delivery-address-container .x-close-icon');
+const change_address_delivery_btn = document.querySelectorAll('.account-address-container .account-change-btn.double .change-btn');
+
+change_address_delivery_btn.forEach(e => {
+    e.addEventListener('click', () => {
+        modal_change_delivery_address.classList.toggle('active');
+        document.body.classList.toggle('open-modal');
+    });
+})
+
+
+change_delivery_address_icon.addEventListener('click', () => {
+    modal_change_delivery_address.classList.toggle('active');
+    document.body.classList.toggle('open-modal'); 
+});
+
+/* Modal Change Personal Data */
+const modal_change_personal_data = document.querySelector('.account-change-personal-data-container');
+const close_change_personal_data_icon = document.querySelector('.account-change-personal-data-container .x-close-icon');
+const change_personal_data_btn = document.querySelector('.account-password-container .account-change-btn .change-btn');
+
+change_personal_data_btn.addEventListener('click', () => {
+    modal_change_personal_data.classList.toggle('active');
+    document.body.classList.toggle('open-modal');
+});
+
+close_change_personal_data_icon.addEventListener('click', () => {
+    modal_change_personal_data.classList.toggle('active');
+    document.body.classList.toggle('open-modal'); 
+});
+
+/* Modal Change Invoice Data */
+const modal_change_invoice_data = document.querySelector('.account-change-invoice-data-container');
+const close_change_invoice__data_icon = document.querySelector('.account-change-invoice-data-container .x-close-icon');
+const change_invoice__data_btn = document.querySelector('.account-invoice-container .account-change-btn .change-btn');
+
+change_invoice__data_btn.addEventListener('click', () => {
+    modal_change_invoice_data.classList.toggle('active');
+    document.body.classList.toggle('open-modal');
+});
+
+close_change_invoice__data_icon.addEventListener('click', () => {
+    modal_change_invoice_data.classList.toggle('active');
+    document.body.classList.toggle('open-modal'); 
+});
+
+/* Modal Change Password */
+const modal_change_password = document.querySelector('.account-change-password-container');
+const close_change_password_icon = document.querySelector('.account-change-password-container .x-close-icon');
+const change_password_btn = document.querySelector('.account-password-container .account-pass-cont-right .account-change-btn .change-btn');
+
+change_password_btn.addEventListener('click', () => {
+    modal_change_password.classList.toggle('active');
+    document.body.classList.toggle('open-modal');
+});
+
+close_change_password_icon.addEventListener('click', () => {
+    modal_change_password.classList.toggle('active');
     document.body.classList.toggle('open-modal'); 
 });
