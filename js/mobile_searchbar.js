df313905cc458ea98d46ec6b9ecfd1421e1dea4b
devlancer0328 <topdev0328@proton.me>
2023-08-03 09:14:00 +0200

    handle searchbar on mobile

---
+++ js/mobile_searchbar.js
@@ -0,0 +1,12 @@
+const mobile_search = document.querySelector('header #nav .top-nav .search-bar-wrapper .mobile_search-btn');
+const nav = document.querySelector('header #nav');
+const search_select = document.getElementById('searchQuerySelect');
+const search_submit = document.getElementById('searchQuerySubmit');
+const search_input = document.getElementById('searchQueryInput');
+
+mobile_search.addEventListener('click', () => {
+    nav.classList.toggle('show-mobile-search-bar');
+    search_select.classList.toggle('active');
+    search_submit.classList.toggle('active');
+    search_input.classList.toggle('active');
+});
