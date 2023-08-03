1528c199623edb3d049d79defe53c89701162baf
devlancer0328 <topdev0328@proton.me>
2023-08-03 09:22:53 +0200

    fix searchbar and mobile menu bug

--- js/script.js
+++ js/script.js
@@ -3,11 +3,13 @@ const menu = document.querySelector('header #nav .bottom-nav');
 const menu_li = document.querySelectorAll('header #nav .bottom-nav .left-container ul li .bottom-nav-sublist > ul > li');
 const footer_year = document.querySelector('.footer-bottom span');
 const header = document.querySelector('body header');
+const search_mobile_bar = document.querySelector('header #nav .top-nav .search-bar-wrapper');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     menu.classList.toggle('active');
     header.classList.toggle('menu-open');
+    search_mobile_bar.classList.toggle('z-index');
     window.scrollTo(0,0);
 });
 
